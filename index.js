38ffa0dd9aec584367595368263090cbbad080c3
Daniyar Seferov <seferovdaniyar@gmail.com>
2018-01-16 08:28:11 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function takeANumber(katzDeliLine, name) {
+  var position = 0;
+  for (let i = 0; i < katzDeliLine.length; i++) {
+    if (katzDeliLine[i] == name) {
+      position = i + 1
+    }
+  }
+  //console.log(position)
+  return `Welcome, ${name}. You are number ${position} in line.`
+}
+
+var katzDeliLine = ['Ada'];
+console.log(takeANumber(katzDeliLine, "Ada"))
