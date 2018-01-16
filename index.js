f1f6f9cbd44898aefa2c08d9a88423e74882a994
Daniyar Seferov <seferovdaniyar@gmail.com>
2018-01-16 09:18:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 function takeANumber(katzDeliLine, name) {
+  
+  if (position === 0) {
+    katzDeliLine.push(name)
+  }
+  //console.log(position)
+  return `Welcome, ${name}. You are number ${position} in line.`
+}
+
+function checkPosition(katzDeliLine, name) {
   var position = 0;
   for (let i = 0; i < katzDeliLine.length; i++) {
     if (katzDeliLine[i] == name) {
       position = i + 1
     }
   }
-  if (position === 0) {
-    katzDeliLine.push(name)
-  }
-  //console.log(position)
-  return `Welcome, ${name}. You are number ${position} in line.`
+  return position
 }
 
 var katzDeliLine = ['Ada'];
