74e4e93963caee099aa006bc1bd9deab2f0c37a8
Daniyar Seferov <seferovdaniyar@gmail.com>
2018-01-16 09:16:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function takeANumber(katzDeliLine, name) {
-  var position = 1;
+  var position = 0;
   for (let i = 0; i < katzDeliLine.length; i++) {
     if (katzDeliLine[i] == name) {
       position = i + 1
     }
+  }
+  if (position === 0) {
+    
   }
   //console.log(position)
   return `Welcome, ${name}. You are number ${position} in line.`
